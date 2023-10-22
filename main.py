bf4040ed76ace0ed38cabfd7b784b8e65ae37e8e
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-10-22 18:57:15 -0300

    att

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import speech_recognition as sr
+import PIL
+from model import charToArray, asciicodes, brailles
 
 rec = sr.Recognizer()
 # print(sr.Microphone().list_microphone_names())
@@ -12,3 +14,14 @@ with sr.Microphone(1) as mic:
     texto = rec.recognize_google(audio, language="pt-BR")
 
     print(texto)
+
+ascii_braille = {}
+
+texto = len(asciicodes)
+counter = 0
+
+while counter < texto:
+    ascii_braille[asciicodes[counter]] = brailles[counter]
+    counter = counter + 1
+
+print(texto)
